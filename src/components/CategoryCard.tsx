009954f85fd64876853729b8286b6bb9ae7c9443
Simon <raess.simon@gmail.com>
2026-01-06 20:05:44 +0100

    feat: use other icons on category

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,3 +1,4 @@
+import { Route, TrendingUp } from 'lucide-react';
 import RunnersBadge from './RunnersBadge';
 
 interface CategoryCardProps {
@@ -17,10 +18,11 @@ function CategoryCard({ name, controls, distance, elevation, runnerCount }: Cate
         </h4>
         <span className="text-sm text-text-tertiary">
           <svg className="inline-block w-4 h-4 -mt-0.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <polygon points="3,3 21,3 3,21" fill="white"/>
-            <polygon points="21,3 21,21 3,21" fill="#FF6B35"/>
-            <rect x="3" y="3" width="18" height="18" fill="none" stroke="#FF6B35" strokeWidth="1.5"/>
-          </svg> {controls} • ↦ {distance}m • ↥ {elevation}m
+            <polygon points="3,3 21,3 3,21" fill="white" />
+            <polygon points="21,3 21,21 3,21" fill="#FF6B35" />
+            <rect x="3" y="3" width="18" height="18" fill="none" stroke="#FF6B35" strokeWidth="1.5" />
+          </svg> {controls}
+          &nbsp; <Route style={{ display: 'inline', width: '16px' }} /> {distance}m &nbsp; <TrendingUp style={{ display: 'inline', width: '16px' }} /> {elevation}m
         </span>
       </div>
       <RunnersBadge count={runnerCount} />
